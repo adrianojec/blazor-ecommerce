7c2d78914d50f4e0c806c863aa3d8db9419f98ea
adrianojec <adrianojeric.28@gmail.com>
2023-04-16 23:05:52 +0800

    Initial commit

---
+++ BlazorECommerce/Client/Shared/ProductList.razor.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http.Json;
+using BlazorECommerce.Shared.Models;
+
+namespace BlazorECommerce.Client.Shared
+{
+   public partial class ProductList
+   {
+      private static List<Product> Products = new List<Product>();
+
+      protected override async Task OnInitializedAsync()
+      {
+         var result = await Http.GetFromJsonAsync<List<Product>>("api/Products");
+         if (result != null)
+            Products = result;
+      }
+   }
+}
+
